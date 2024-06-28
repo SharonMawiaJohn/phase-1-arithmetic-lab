12cd8a1c17eb61fb4642b054f1bfb2ac61c2d5ea
SharonMawiaJohn <sharonmawia76@gmail.com>
2024-06-28 10:55:53 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2
+multiply
+const random = Math.floor(Math.random()*10) +1
+random
+const num3=9
+const num4=5
+const mod = num3%num4
+let max = Math.max(1, 3, 5, 7, 11, 20);
 
-// Write your code here
+console.log(multiply);
+console.log(random); 
+console.log(mod);
+console.log(max); 
